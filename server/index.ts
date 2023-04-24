e5dd328c6f2f29c3c6f9bf235727841c097f5dfb
tsuki917 <einstrom917@gmail.com>
2023-04-24 15:44:12 +0900

    client

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,15 @@
+const express = require("express");
+const app =express();
 
+const http = require("http");
+const server = http.createServer(app);
+const {Server} = require("socket.io");
+
+const io = new Server(server);
+
+const PORT = 5000;
+
+io.on("connection",(socket)=>{
+    console.log("クライアントと接続");
+    socket.on("get_position")
+})
