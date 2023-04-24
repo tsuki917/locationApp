a8d167b3419b009d1b36c6e133cce7f22e36cbfd
tsuki917 <einstrom917@gmail.com>
2023-04-24 21:41:29 +0900

    basic connect test

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,23 @@
-const express = require("express");
-const app =express();
+const  express = require("express");
+const app = express();
 
-const http = require("http");
+import http from "http";
 const server = http.createServer(app);
-const {Server} = require("socket.io");
+import { Server } from "socket.io";
 
-const io = new Server(server);
+const io = new Server(server,{
+    cors:{
+        origin:["http://localhost:3000"],
+    },
+});
 
 const PORT = 5000;
 
-io.on("connection",(socket)=>{
+io.on("connection", (socket) => {
     console.log("クライアントと接続");
-    socket.on("get_position")
+
+})
+
+server.listen(PORT, () => {
+    console.log(`server running on ${PORT}`);
 })
