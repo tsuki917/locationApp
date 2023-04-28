24e5bfd3f2590926bf272e2d6bc96cb930205e06
tsuki917 <einstrom917@gmail.com>
2023-04-28 21:58:30 +0900

    Revert "test"
    This reverts commit 76ecd170663796bd8fb89881b46e89734cbb9fdb.

--- client/component/Map.tsx
+++ client/component/Map.tsx
@@ -1,4 +1,5 @@
-import { GoogleMap, LoadScript,Marker, useLoadScript } from "@react-google-maps/api";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+
 const containerStyle = {
   width: "800px",
   height: "400px",
@@ -8,16 +9,16 @@ const center = {
   lat: 35.69575,
   lng: 139.77521,
 };
+
 const MyComponent = () => {
-//   if(process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY){
-//   const {isLoaded} = useLoadScript({
-//     googleMapsApiKey:process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY,
-//   });
-// }
-
-
   return (
-    <div id="map"></div>
+    <LoadScript googleMapsApiKey="AIzaSyAheiUVYAXMXnpaIjFQCczhVUUEe39NhLc">
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={17}
+      ></GoogleMap>
+    </LoadScript>
   );
 };
 
