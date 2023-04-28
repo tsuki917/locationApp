76ecd170663796bd8fb89881b46e89734cbb9fdb
tsuki917 <einstrom917@gmail.com>
2023-04-28 21:58:24 +0900

    test

--- client/component/Map.tsx
+++ client/component/Map.tsx
@@ -1,5 +1,4 @@
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
-
+import { GoogleMap, LoadScript,Marker, useLoadScript } from "@react-google-maps/api";
 const containerStyle = {
   width: "800px",
   height: "400px",
@@ -9,16 +8,16 @@ const center = {
   lat: 35.69575,
   lng: 139.77521,
 };
-
 const MyComponent = () => {
+//   if(process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY){
+//   const {isLoaded} = useLoadScript({
+//     googleMapsApiKey:process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY,
+//   });
+// }
+
+
   return (
-    <LoadScript googleMapsApiKey="AIzaSyAheiUVYAXMXnpaIjFQCczhVUUEe39NhLc">
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={17}
-      ></GoogleMap>
-    </LoadScript>
+    <div id="map"></div>
   );
 };
 
