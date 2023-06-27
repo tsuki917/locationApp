90d79e1e5cce65cdbdcfb109d05a1b581bb62af2
tsuki917 <einstrom917@gmail.com>
2023-06-27 20:00:19 +0900

    to revert

---
+++ client/component/Menu.tsx
@@ -0,0 +1,11 @@
+import { NextRouter,Router,useRouter } from "next/router";
+
+export default function Menu(){
+    const test = "test";
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
