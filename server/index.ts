81c8e19082e009273c773314740bee03d841e428
tsuki917 <einstrom917@gmail.com>
2023-08-03 13:04:17 +0900

    add function delete add get for firebase

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-import { collection, addDoc ,setDoc,doc} from "firebase/firestore"; 
+import { collection, addDoc ,setDoc,doc,getDocs,deleteDoc} from "firebase/firestore"; 
 import { getFirestore } from "firebase/firestore";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
@@ -23,22 +23,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const appFire = initializeApp(firebaseConfig);
 const db = getFirestore(appFire);
-async function docTest(){
-    console.log("docTest")
-    try {
-        console.log("test")
-        const docRef = await addDoc(collection(db, "test"), {
-            first: "Ada",
-            last: "Lovelace",
-            born: 1815
-          });
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
-    console.log("test");
-    
-}
-docTest();
 
 
 
@@ -73,20 +57,25 @@ type socketDataType = {
       lng: number
     }
     
-  }
+}
 const PORT = 5000;
 const socketData_array: socketDataType[] = [];
 io.on("connection", (socket) => {
-    console.log("クライアントと接続");
     const socket_id = socket.id;
-    socket.emit('newClient',socketData_array);
-    socketData_array.push({name:'noName',position:{lat:0,lng:0},id:socket_id,roomId:"",selfIntroduce:'よろしくお願いします！'});
+    let roomId_stg:string;
+    // socket.emit('newClient',socketData_array);
+    // socketData_array.push();
+    
+    
+    
     socket.emit("init",socket_id);
     socket.on("init_res",(socketData:socketDataType)=>{
         if(socketData.roomId!==undefined){
             socket.join(socketData.roomId);
+            roomId_stg = socketData.roomId;
+            socket.emit("newClient",getRoomCollection(socketData));
+            // setRoomCollection(socketData.roomId,socket_id,socketData);
             console.log(socketData.roomId+"に入室しました");
-
         }else{
             console.log("roomIdがundefined");
         }
@@ -108,23 +97,35 @@ io.on("connection", (socket) => {
 
     socket.on("changeData",(newData:socketDataType)=>{
         if(newData.id!==''){
-            searchId(newData);
-            console.log("change");
-            socket.to(newData.roomId).emit("send_AllClientData",socketData_array);
+            setRoomCollection(newData);
+            socket.to(newData.roomId).emit("send_AllClientData");
+            
+            
         }else{
             socket.emit("init",socket_id);
             console.log("false");
         }
+
+
+
+
+        // if(newData.id!==''){
+        //     searchId(newData);
+        //     console.log("change");
+        //     socket.to(newData.roomId).emit("send_AllClientData",socketData_array);
+        // }else{
+        //     socket.emit("init",socket_id);
+        //     console.log("false");
+        // }
     });
-    socket.on("disconnect",()=>{
-        console.log(socketData_array);
-        for(let i=0;i<socketData_array.length;i++){
-            if(socketData_array[i].id===socket_id){
-                socketData_array.splice(i,1);
-                console.log(socketData_array);
-            }
-        }
-        console.log(socket_id);
+    socket.on("disconnect",(data)=>{
+        // for(let i=0;i<socketData_array.length;i++){
+        //     if(socketData_array[i].id===socket_id){
+        //         socketData_array.splice(i,1);
+        //         console.log(socketData_array);
+        //     }
+        // }
+        deleteClientData(socket_id,roomId_stg)
     });
 
 });
@@ -134,19 +135,103 @@ server.listen(PORT, () => {
 });
 
 
-function searchId(insertData:socketDataType){
-    for(let i = 0;i<socketData_array.length;i++){
-        if(socketData_array[i].id===insertData.id){
-            socketData_array[i].name=insertData.name;
-            socketData_array[i].position.lat=insertData.position.lat;
-            socketData_array[i].position.lng=insertData.position.lng;
-            socketData_array[i].selfIntroduce=insertData.selfIntroduce;
-            socketData_array[i].roomId = insertData.roomId;
-            console.log(socketData_array[i]);
+// function searchId(insertData:socketDataType){
+//     for(let i = 0;i<socketData_array.length;i++){
+//         if(socketData_array[i].id===insertData.id){
+//             socketData_array[i].name=insertData.name;
+//             socketData_array[i].position.lat=insertData.position.lat;
+//             socketData_array[i].position.lng=insertData.position.lng;
+//             socketData_array[i].selfIntroduce=insertData.selfIntroduce;
+//             socketData_array[i].roomId = insertData.roomId;
+//             console.log(socketData_array[i]);
 
-        }
-    }
+//         }
+//     }
+// }
+const data1 = {
+    name:'noName',
+    position:{lat:0,lng:0},
+    id:"mongo",
+    roomId:"room1",
+    selfIntroduce:'よろしくお願いします！'
+}
+const data2 = {
+    name:'noName',
+    position:{lat:0,lng:0},
+    id:"firebase",
+    roomId:"room1",
+    selfIntroduce:'よろしくお願いします！'
+}
+const data3 = {
+    name:'noName',
+    position:{lat:0,lng:0},
+    id:"SQlite",
+    roomId:"room1",
+    selfIntroduce:'よろしくお願いします！'
 }
 
+// setRoomCollection(data3);
+const allClients = getRoomCollection(data1).then((data1)=>console.log(data1));
+console.log(allClients);
+// deleteClientData(data1);
+
+/*
+ClientsRooms
+    roomId
+        [roomId]
+            [socketId]
+                [Data]
 
 
+
+*/
+
+//クライアントデータを追加する
+async function setRoomCollection(socketData:socketDataType){
+    console.log("docTest");
+    const room = doc(db,"ClientsRooms","roomId",socketData.roomId,socketData.id);
+    
+    try {
+        await setDoc(room,socketData);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+}
+//roomIdの部屋の全てのデータを取得する
+async function getRoomCollection(data:socketDataType){
+    const allClientsData : socketDataType[] = [];
+    const q =collection(db,"ClientsRooms","roomId",data.roomId);
+    const querySnap = await getDocs(q);
+    
+    querySnap.forEach(ele =>{
+        const domData :socketDataType={
+            name:'',
+            position:{lat:0,lng:0},
+            id:"",
+            roomId:"",
+            selfIntroduce:''
+        }
+
+        
+        const gotData = ele.data();
+        domData.id = gotData.id;
+        domData.name  = gotData.name;
+        domData.position  = gotData.position;
+        domData.roomId = gotData.roomId;
+        domData.selfIntroduce = gotData.selfIntroduce;
+            
+        allClientsData.push(domData);   
+
+        
+    });
+    return allClientsData;
+    
+}
+
+//dbから特定のクライアントデータを削除する
+async function deleteClientData(socket_id:string,roomId:string){
+    
+    await deleteDoc(doc(db,"ClientsRooms","roomId",roomId,socket_id));
+
+
+}
