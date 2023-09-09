b1f242b19b2b0bce3ae569be3e28e684747fda93
tsuki917 <einstrom917@gmail.com>
2023-09-09 18:17:10 +0900

    serverURL change

--- client/component/Map.tsx
+++ client/component/Map.tsx
@@ -23,7 +23,7 @@ let serverURL;
 if (environment === "development") {
   serverURL = "http://localhost:5000";
 } else if (environment === "production") {
-  serverURL = "https://location-app-ten.vercel.app";
+  serverURL = "https://location-server-f9tv.onrender.com";
 }
 console.log("serverURL:" + serverURL);
 let socket: Socket;
